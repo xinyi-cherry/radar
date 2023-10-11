573b97df2dc7610834a88f3baf8d07d8eb61415d
taitanium <fj889910@163.com>
2023-10-11 20:01:44 +0800

    change ft dpi

--- FT_jpghandler.py
+++ FT_jpghandler.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2 
 
-ISDEBUG = False
+ISDEBUG = True
 
 def mainEdgeGetter(inImg:np.ndarray)->np.ndarray:
     #dbg
@@ -77,7 +77,7 @@ class debugPlot:
 
 
 if __name__ == '__main__':
-    img=cv2.imread('./output/figs/cth_10_11_Raw_0_FT_768_1.jpg')
+    img=cv2.imread('./output/figs/data1_1_Raw_0_FT_512_1.jpg')
     img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
     rt=mainEdgeGetter(img)
