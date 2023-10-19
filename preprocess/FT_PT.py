5c6823ad061d3a391a1e4ba217ad31677a529ce5
xinyi-cherry <1316010781@qq.com>
2023-10-19 16:09:58 +0800

    update FT image

--- preprocess/FT_PT.py
+++ preprocess/FT_PT.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import scipy.io as io
 from scipy import signal
 import os
+import tqdm
 
 def double_exponential_smoothing(series, alpha, beta):
     """
@@ -68,6 +69,7 @@ def phase(type,file_position, file_num, file_name, adcData, num_ADCSamples = 128
     num = eval("1e-"+str(4+int(np.real(np.max(data_fft))//300)))
     num = 1e-5
     print(max_range)
+    dataTest = []
     for k in range(num_frame):
         for m in range(num_mti):
             data_fft[m,:,k] = np.fft.fft(data_mti[m,:,k])
@@ -76,20 +78,39 @@ def phase(type,file_position, file_num, file_name, adcData, num_ADCSamples = 128
             #print(np.real(max(data_fft[m,:,k])))
             # TH_cfar[m, :, k] = cfar(abs(data_fft[m, :, k]),num)
             # data_fft[m,:,k] = (TH_cfar[m,:,k] < abs(data_fft[m,:,k])) * (data_fft[m,:,k])
+            dataTest.append(data_fft[m,data_index,k])
             
-            if max(abs(data_fft[m,:,k]))<max_range*1.1:
-                if k!=0:
-                    angle_fft[m,k] = angle_fft[m,k-1]
-                else:
-                    angle_fft[m,k] = angle_fft[m-1,-1]
             #else:
                 #plt.plot(range(num_ADCSamples),data_fft[m,:,k])
             data_index = np.argmax(abs(data_fft[m,:,k]))
             data_angle = np.angle(data_fft[m,data_index,k])
             angle_fft[m,k] = data_angle
+            if max(abs(data_fft[m,:,k]))<max_range*1.1:
+                if k!=0:
+                    angle_fft[m,k] = angle_fft[m,k-1]
+                else:
+                    angle_fft[m,k] = angle_fft[m-1,-1]
         #break
     #plt.show()
     #plt.close()
+    def dacm(dt:np.ndarray)->np.ndarray:
+        lent=dt.shape[0]
+        print(lent)
+        ret= []
+        img=np.imag(dt).tolist()
+        rel=np.real(dt).tolist()
+        ans = 0
+        for j in tqdm.tqdm(range(1, lent)):
+            ans+=((rel[j]*(img[j]-img[j-1])-img[j]*(rel[j]-rel[j-1]))/(rel[j]**2+img[j]**2))
+            ret.append(ans)
+        return ret
+    plt.figure()
+    plt.plot(range(len(dataTest)-1),dacm(np.array(dataTest)))
+    plt.axis('off')
+    plt.margins(0,0)
+    plt.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
+    plt.savefig(file_position+file_name+'_newPT_'+str(file_num)+'.jpg', pad_inches=0)
+    plt.close()
     print(file_name)
     stft_data = np.asarray([])
     for k in range(num_frame):
